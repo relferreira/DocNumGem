790405a8b5b853471d49a0ffcc1b167dd7ed4461
Renan Lopes Ferreira <relferreira@gmail.com>
2016-03-02 23:49:28 -0300

    RecyclerView in the favorites tab

---
+++ app/src/main/java/com/relferreira/docnumgen/main/DocFavAdapter.java
@@ -0,0 +1,52 @@
+package com.relferreira.docnumgen.main;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.relferreira.docnumgen.R;
+import com.relferreira.docnumgen.model.Doc;
+
+import java.util.List;
+
+/**
+ * Created by renan on 02/03/2016.
+ */
+public class DocFavAdapter extends RecyclerView.Adapter<DocFavAdapter.DocFavViewHolder> {
+
+
+    private final List<Doc> documents;
+
+    public DocFavAdapter(List<Doc> documents) {
+        this.documents = documents;
+    }
+
+    @Override
+    public DocFavViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View item = LayoutInflater.from(parent.getContext()).inflate(R.layout.adapter_docs_fav, parent, false);
+        return new DocFavViewHolder(item);
+    }
+
+    @Override
+    public void onBindViewHolder(DocFavViewHolder holder, int position) {
+        Doc document = documents.get(position);
+        holder.docText.setText(document.getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return documents.size();
+    }
+
+    public static class DocFavViewHolder extends RecyclerView.ViewHolder {
+
+        protected TextView docText;
+
+        public DocFavViewHolder(View itemView) {
+            super(itemView);
+            docText = (TextView) itemView.findViewById(R.id.adapter_fav_text);
+        }
+    }
+}
--- app/src/main/java/com/relferreira/docnumgen/main/DocFavFragment.java
+++ app/src/main/java/com/relferreira/docnumgen/main/DocFavFragment.java
@@ -3,6 +3,8 @@ package com.relferreira.docnumgen.main;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -25,6 +27,11 @@ public class DocFavFragment extends Fragment implements DocTabFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_doc_fav, container, false);
+
+        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.docs__fav_list);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
 
         return view;
     }
